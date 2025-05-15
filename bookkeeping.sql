ccdd2b9825e08ea382c7268e529d2781a27b91ba
Shai Gerbi <ShaiGerbi@gmail.com>
2025-05-15 23:39:56 +0300

    Add PaymentTerms table for handling net payment logic

--- bookkeeping.sql
+++ bookkeeping.sql
@@ -4,3 +4,16 @@
 CREATE DATABASE bookkeeping;
 GO
 USE bookkeeping;
+
+
+/*
+טבלת תנאי תשלום
+שוטף - דורש ללכת לסוף החודש ולהוסיף תקופת זמן
+*/
+CREATE TABLE PaymentTerms(
+    PaymentTermId INT PRIMARY KEY IDENTITY,
+	PaymentTermName NVARCHAR(32) UNIQUE NOT NULL,
+	Months TINYINT,
+	Days TINYINT,
+	NeedsEndOfMonth BIT
+);
